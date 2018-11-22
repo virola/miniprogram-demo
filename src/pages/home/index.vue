456b138d97e7f05359d80f0c9427c957b3085002
Virola <virola@163.com>
2018-11-22 16:46:59 +0800

    project init

---
+++ src/pages/home/index.vue
@@ -0,0 +1,59 @@
+<template>
+
+<view class="home-wrap">
+  <hello :from="from" @hello="handleHello"></hello>
+  <view class="click-tip" if="clicked">You click me~</view>
+  <import src="../../common/tpl/footer.tpl" />
+  <tpl is="page-footer" />
+</view>
+</template>
+
+<script>
+import Hello from '../../components/Hello';
+
+export default {
+  config: {
+    title: '首页'
+  },
+
+  components: {
+    Hello
+  },
+
+  data: {
+    btnText: 'Hello',
+    clicked: false,
+    from: 'HomePage'
+  },
+
+  methods: {
+
+    handleHello(e) {
+      this.clicked = true;
+      this.btnText = 'You clicked';
+
+      this.$api.showToast({
+        title: 'Received Hello',
+        duration: 3000
+      });
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+@require '../../common/css/variable.styl'
+@require '../../common/css/mixin.styl'
+
+.home-wrap
+  padding: 100px
+  height: 100vh
+  box-sizing: border-box
+  background: #ddd
+
+.click-tip
+  margin-top: 100px
+  padding: 20px
+  text-align: center
+  color: red
+</style>
